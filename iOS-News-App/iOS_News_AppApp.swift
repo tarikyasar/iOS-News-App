b441f8b45257d76ac6c67ff86a964aa621c6c6b0
Tarik Yasar <m.tarikyasar@gmail.com>
2023-08-12 20:07:39 +0300

    Initial Commit

---
+++ iOS-News-App/iOS_News_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_News_AppApp.swift
+//  iOS-News-App
+//
+//  Created by Tarik Yasar on 12.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct iOS_News_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
